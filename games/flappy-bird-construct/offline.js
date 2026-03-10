88ad9243ca93ba0a83b9a725f5342108fad35e5d
Fleig <fleig.productions@gmail.com>
2026-03-10 12:08:47 -0300

    added new game

---
+++ games/flappy-bird-construct/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1773155050,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/ceiling-sheet0.png",
+		"images/toppipe-sheet0.png",
+		"images/bottompipe-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
