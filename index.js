0ca2ecf93ba7a45348b685278caad2065275ecd4
Fleig <fleig.productions@gmail.com>
2026-03-11 15:24:18 -0300

    added fun fact of the day (jesus that was hard)

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const factsCount = 5;
+	const today = new Date();
+	const dayOfYear = Math.floor((today - new Date(today.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+	const factId = (dayOfYear - 1) % factsCount + 1;
+	
+	fetch(`https://fleig.dev/?id=${factId}`)
+    .then(response => response.text())
+    .then(fact => {
+      document.getElementById('fact').textContent = fact;
+    })
+    .catch(err => {
+      console.error("Error fetching fact:", err);
+      document.getElementById('fact').textContent = "Error loading fact.";
+    });
+});
+
 window.onload = function() {
     const names = ["Matt", "Matthew", "Matheus", "Matias", "Suehtam", "Mateo", "Matthäus", "Matthaios", "Matti", "Matthaeus", "M"]
 	const randint = Math.floor(Math.random() * names.length);
